be25ccedb29387b4928554bd58935230e6de5e42
Masatomi KINO <masatomix@ki-no.org>
2024-03-14 05:51:46 +0000

    Container Insight の設定を追加

--- lib/ClusterStack.ts
+++ lib/ClusterStack.ts
@@ -3,6 +3,7 @@ import { getProfile } from './Utils'
 import { CfnCluster } from 'aws-cdk-lib/aws-ecs'
 import { CfnApplication } from 'aws-cdk-lib/aws-codedeploy'
 
+
 export class ClusterStack extends Stack {
   public readonly cluster: CfnCluster
   public readonly application: CfnApplication
@@ -14,6 +15,9 @@ export class ClusterStack extends Stack {
 
     this.cluster = new CfnCluster(this, `${server}-cluster`, {
       clusterName: `${server}-cluster`,
+      clusterSettings: [
+        { name: 'containerInsights', value: 'enabled' }
+      ]
     })
     this.application = new CfnApplication(this, `${server}-Application`, {
       applicationName: `${server}-app`,
